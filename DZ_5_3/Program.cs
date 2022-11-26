2423b7b75662a5c10d445cd0145b17b4a82b1af1
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-26 13:24:59 +0300

    DZ_5_3

---
+++ DZ_5_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray(int[] NumberArray)
+{
+    for (int i = 0; i < NumberArray.Length; i++)
+    {
+        NumberArray[i] = new Random().Next(-9, 10);
+    }
+}
+
+void FormattedPrintArray(int[] NumberArray)
+{
+    string[] result = Array.ConvertAll(NumberArray, x => x.ToString());
+    Console.WriteLine($"[ {String.Join(", ", result)} ]");
+}
+
+void FindMinMaxInArray(int[] NumberArray)
+{
+    int min = 0;
+    int max = 0;
+    for (int i = 0; i < NumberArray.Length; i++)
+    {
+        if (NumberArray[i] > max)
+        {
+           max = NumberArray[i];
+        }
+        if (NumberArray[i] < min)
+        {
+           min = NumberArray[i];
+        }
+    }
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {max-min}");
+}
+
+int[] array = new int [5];
+
+FillArray(array);
+
+Console.WriteLine();
+FormattedPrintArray(array);
+Console.WriteLine();
+
+FindMinMaxInArray(array);
+Console.WriteLine();
