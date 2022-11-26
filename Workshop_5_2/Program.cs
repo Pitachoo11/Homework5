d4ea1602a592c2ba23b8b3dec654d292cdc9d2f7
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-26 10:25:12 +0300

    Workshop_5_2

---
+++ Workshop_5_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные,
+// и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void FillArray(int[] NumberArray)
+{
+    for (int i = 0; i < NumberArray.Length; i++)
+    {
+        NumberArray[i] = new Random().Next(-9, 10);
+    }
+}
+
+void FormattedPrintArray(int[] NumberArray)
+{
+    string[] result = Array.ConvertAll(NumberArray, x => x.ToString());
+    Console.WriteLine($"[ {String.Join(", ", result)} ]");
+}
+
+void ChangePosNegSignArray(int[] NumberArray)
+{
+    for (int i = 0; i < NumberArray.Length; i++)
+    {
+        NumberArray[i] = NumberArray[i] * -1;
+    }
+}
+
+int[] array = new int [12];
+
+FillArray(array);
+
+Console.WriteLine();
+FormattedPrintArray(array);
+
+ChangePosNegSignArray(array);
+Console.WriteLine();
+
+FormattedPrintArray(array);
+Console.WriteLine();
